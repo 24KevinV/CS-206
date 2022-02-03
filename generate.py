4931b6a1143a7a4949e1672e08c5f28ab2836d28
24KevinV <70664835+24KevinV@users.noreply.github.com>
2022-02-03 10:48:11 -0500

    fixed typo in pyrosim and added sensor to BackLeg

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-import pyrosim.pyrosim as pyroism
+import pyrosim.pyrosim as pyrosim
 
 length = 1
 width = 1
@@ -9,14 +9,14 @@ z = height / 2
 
 
 def Create_World():
-    pyroism.Start_SDF("world.sdf")
+    pyrosim.Start_SDF("world.sdf")
 
-    pyroism.Send_Cube(name="Box", pos=[-3, 3, 0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[-3, 3, 0.5], size=[length, width, height])
 
     # x = 1
     # y = 0
     # z = height + height / 2
-    # pyroism.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Box2", pos=[x, y, z], size=[length, width, height])
     # for i in range(5):
     #     for j in range(5):
     #         length = 1
@@ -24,23 +24,23 @@ def Create_World():
     #         height = 1
     #         z = height / 2
     #         for k in range(10):
-    #             pyroism.Send_Cube(name="Box", pos=[i, j, z], size=[length, width, height])
+    #             pyrosim.Send_Cube(name="Box", pos=[i, j, z], size=[length, width, height])
     #             z += (height + height * 0.9) / 2
     #             length *= 0.9
     #             width *= 0.9
     #             height *= 0.9
 
-    pyroism.End()
+    pyrosim.End()
 
 
 def Create_Robot():
-    pyroism.Start_URDF("body.urdf")
-    pyroism.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[length, width, height])
-    pyroism.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, 1])
-    pyroism.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
-    pyroism.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, 1])
-    pyroism.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
-    pyroism.End()
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.End()
 
 
 Create_World()
--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -11,8 +12,11 @@ robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
     print(i)
 
