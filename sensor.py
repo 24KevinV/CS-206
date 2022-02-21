cc0fd090e30b47e947723065f2d70e029d545c70
24KevinV <70664835+24KevinV@users.noreply.github.com>
2022-02-21 13:35:03 -0500

    removed print statement and commented out code

--- sensor.py
+++ sensor.py
@@ -10,8 +10,6 @@ class SENSOR:
 
     def Get_Value(self, t):
         self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
-        if t == len(self.values) - 1:
-            print(self.values)
 
     def Save_Values(self):
         np.save("data/sensorValues.npy", self.values)
